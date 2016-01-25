24c75571e8014d6ee74b70bb69614404557131c1
Adam Zolotarev <eugenezolo@gmail.com>
2016-01-24 21:49:53 -0500

    multiple cats

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
 var Cat = Cat || {};
 Cat.catClicker = function(){
-	
-	var updateCatClickCounter = function(){
-		var clickerId = this.dataset.counterId,
-			clickCounter = document.getElementById(clickerId),
-			counterValue = clickCounter.dataset.clickCount;
-
-		counterValue++;
-		clickCounter.dataset.clickCount = counterValue;
-		clickCounter.innerHTML = counterValue;
+	'use strict';
+	var clicks = [];
+	var countCatClicks = function(catNumber){
+		var clickCount = clicks[catNumber];
+		if(clickCount === undefined)
+		{
+			clicks[catNumber] = 0;
+		}
+		else{
+			clickCount++;
+			clicks[catNumber] = clickCount;
+		}
+	},
+	createCatPicture = function(catNumber){
+	var catPicture = document.createElement("img");
+		catPicture.src = 'cat' + catNumber + '.jpg';
+		return catPicture;
 	},
 
-	onCatClickIncreaseClickCounter = function(){		
-		var cats = document.getElementsByClassName('cat-picture'),
-			numberOfCats = cats.length;
+	showMyCat = function(){
+		var catNumber = this.dataset.catNumber,
+			catPictureDiv = document.getElementById('cat-picture'),
+			catImage = createCatPicture(catNumber);
+			catPictureDiv.innerHTML = '';
+			catImage.onclick = function(){
+				countCatClicks(catNumber);
+			};
+			catPictureDiv.appendChild(catImage);
 
-		for(var i=0; i<numberOfCats; i++){
-			cats[i].addEventListener('click',updateCatClickCounter);
-		}		
+		return false;
 	},
 
+	giveMeCatListItem = function(catNumber) {
+		var newCatListItem = document.createElement("li"),
+			catLink = document.createElement('a');
+						
+			catLink.href='cat' +catNumber+'.jpg';			
+			catLink.onclick = showMyCat;
+			catLink.innerHTML = 'cat # ' + catNumber;
+			catLink.setAttribute('data-cat-number', catNumber);
+			newCatListItem.appendChild(catLink);
+
+			return newCatListItem;
+	},
+	createCatList = function(numberOfCats){
+		var catList = document.getElementById('cat-list');
+
+		for(var catNumber = 0; catNumber < numberOfCats; catNumber++){
+			catList.appendChild(giveMeCatListItem(catNumber));
+		}
+	},
 	
-	init = function(){
-		onCatClickIncreaseClickCounter();
+	init = function(numberOfCats){
+		createCatList(numberOfCats);
 	};
 
 	return {
@@ -30,4 +61,4 @@ Cat.catClicker = function(){
 	};
 }();
 
-Cat.catClicker.init();
+Cat.catClicker.init(3);
