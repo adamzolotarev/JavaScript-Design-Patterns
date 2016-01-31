22d13a81ade0eccede9ae431c8042ebe2f0e6bd0
Adam Zolotarev <eugenezolo@gmail.com>
2016-01-31 15:46:49 -0500

    Added admin area

--- app.js
+++ app.js
@@ -2,58 +2,106 @@ var Cat = Cat || {};
 Cat.catClicker = function(){
 	'use strict';
 
-	var model = {
+	var model = {		
 		init: function(numberOfCats){
-			this.catClicks = this.catClicks || [];			
-			this.numberOfCats = numberOfCats;
-			this.catName;
+			this.cats = [];
+			for (var i = 0; i < numberOfCats; i++) {
+				this.cats.push({
+					clickCount: 0,
+					name: 'cat' + i,
+					imgSrc: 'cat' + i + '.jpg',
+					id: i
+				});
+			}
 		},
-		incrementCatClicks: function(){
-			this.catClicks[this.currentCatNumber] = (this.catClicks[this.currentCatNumber] || 0) + 1;
-		},
-		setCurrentCat: function(catNumber){
-			this.currentCatNumber = catNumber;
-			this.catName = 'cat#' + catNumber;
-		},
-		getCurrentCatNumber: function(){
-			return this.currentCatNumber;
-		},
-		getCatClickCount: function(){
-			return this.catClicks[model.getCurrentCatNumber()] || 0;
-		},
-		getNumberOfCats: function(){
-			return this.numberOfCats;
-		},
-		getCatName: function(){
-			return this.catName;
-		}
+		currentCat: null,
+		adminMode: false
 	};
 
 	var octopus = {
 		incrementCatClicks: function(){
-			model.incrementCatClicks();
+			model.currentCat.clickCount++;
 			catDisplayView.render();
-		},
-		setCurrentCat: function(catNumber){
-			model.setCurrentCat(catNumber);
-			catDisplayView.render();
-		},
-		getCurrentCatNumber : function(){
-			return model.getCurrentCatNumber();
-		},
-		getClickCount: function(){
-			return model.getCatClickCount();
-		},
-		getNumberOfCats: function(){
-			return model.getNumberOfCats();
-		},
-		getCatName: function(){
-			return model.getCatName();
 		},		
+		setCurrentCat: function(cat){
+			model.currentCat = cat;						
+			catDisplayView.render();
+		},				
+		getCurrentCat: function(){
+			return model.currentCat;
+		},
+		getCats: function(){
+			return model.cats;
+		},
+
+		saveAdminCat: function(cat){			
+			var matchingCat = model.cats.filter(function(existingCat){
+				return existingCat.id === cat.id;
+			})[0];
+			matchingCat.name = cat.name;
+			matchingCat.clickCount = cat.clickCount;
+			matchingCat.imgSrc = cat.imgSrc;
+			octopus.setCurrentCat(matchingCat);
+		},
+		setAdminMode: function(){
+			model.adminMode = true;
+		},
+		setUserMode: function(){
+			model.adminMode = false;
+		},
+		isAdminMode: function(){
+			return model.adminMode;
+		},
+
 		init: function(numberOfCats){
 			model.init(numberOfCats);
 			catListView.init();
-			catDisplayView.init();			
+			catDisplayView.init();
+			adminView.init();
+		}
+	};
+
+	var adminView = {
+		init: function(){
+			this.adminArea = document.getElementById('admin-area');
+			this.adminButton = document.getElementById('admin-button');
+			this.adminSaveButton = document.getElementById('admin-save-button');
+			this.adminCancelButton = document.getElementById('admin-cancel-button');
+			this.adminCatName = document.getElementById('admin-cat-name');
+			this.adminCatClicks = document.getElementById('admin-cat-clicks');
+			this.adminCatUrl = document.getElementById('admin-cat-url');
+
+			this.adminButton.addEventListener('click', function(){
+				octopus.setAdminMode();
+				adminView.render();
+			});
+			this.adminCancelButton.addEventListener('click', function(){
+				octopus.setUserMode();
+				adminView.render();
+			});
+			this.adminSaveButton.addEventListener('click', function(){
+				octopus.saveAdminCat({
+					clickCount: adminView.adminCatClicks.value,
+					name: adminView.adminCatName.value,
+					imgSrc: adminView.adminCatUrl.value,
+					id: octopus.getCurrentCat().id});
+
+				octopus.setUserMode();
+				adminView.render();
+			});
+		},
+		render: function(){
+			var cat;
+			if(octopus.isAdminMode()){
+				this.adminArea.style.display = '';
+				cat = octopus.getCurrentCat();
+				this.adminCatName.value = cat.name;
+				this.adminCatClicks.value = cat.clickCount;
+				this.adminCatUrl.value = cat.imgSrc;
+			}
+			else{
+				this.adminArea.style.display = 'none';
+			}
 		}
 	};
 
@@ -62,35 +110,37 @@ Cat.catClicker = function(){
 			this.catImage = document.getElementById('cat-picture');
 			this.catClickCounter = document.getElementById('click-counter');			
 			this.catNameArea = document.getElementById('cat-name');
+			this.adminButton = document.getElementById('admin-button');
 			this.catImage.addEventListener('click', function(e){
 				octopus.incrementCatClicks();
 				e.preventDefault;
 			});
+
 			catDisplayView.render();
 		},
 		render: function(){
-			var catNumber = octopus.getCurrentCatNumber();
+			var currentCat = octopus.getCurrentCat();
 
-			if(catNumber === undefined) return;
-			
-			this.catImage.src = 'cat' + octopus.getCurrentCatNumber() + '.jpg';			
-			this.catNameArea.innerHTML = 'My name is ' + octopus.getCatName();		
-			this.catClickCounter.innerHTML = 'You clicked me ' + octopus.getClickCount() + ' times';
+			if(!currentCat) return;			
+			this.adminButton.style.display = '';
+			this.catImage.src = currentCat.imgSrc;			
+			this.catNameArea.innerHTML = currentCat.name;		
+			this.catClickCounter.innerHTML = 'You clicked me ' + currentCat.clickCount + ' times';
 		}
 	};
 
 	var catListView = {
-		giveMeCatListItem: function(catNumber) {
+		giveMeCatListItem: function(cat) {
 			var newCatListItem = document.createElement("li"),
 				catLink = document.createElement('a');
 							
 				catLink.addEventListener('click', function(e){
 					e.preventDefault();
 					e.stopPropagation();
-					octopus.setCurrentCat(catNumber);					
+					octopus.setCurrentCat(cat);					
 				});
 
-				catLink.innerHTML = 'cat # ' + catNumber;				
+				catLink.innerHTML = cat.name;				
 				newCatListItem.appendChild(catLink);
 
 				return newCatListItem;
@@ -100,10 +150,12 @@ Cat.catClicker = function(){
 			catListView.render();
 		},
 		render: function(){
-			var numberOfCats = octopus.getNumberOfCats();
+			var cats = octopus.getCats(),
+				catNumber,
+				numberOfCats = cats.length;
 
-			for(var catNumber = 0; catNumber < numberOfCats; catNumber++){
-				this.catList.appendChild(catListView.giveMeCatListItem(catNumber));
+			for(catNumber = 0; catNumber < numberOfCats; catNumber++){
+				this.catList.appendChild(catListView.giveMeCatListItem(cats[catNumber]));
 			}
 		}
 	};
